b872573c66ecabbaf42973dd8f98a933ae0a1359
RadinAjoodani <radin.ajoodani@gmail.com>
2025-07-04 01:11:20 +0330

    first project with django

---
+++ base/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Room
+# Create your views here.
+
+rooms = [
+    {'id':1, 'name':'radin\'s room'},
+    {'id':2, 'name':'kitchen'},
+    {'id':3, 'name':'bathroom'}
+]
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
+    return render(request, 'base/room.html', context)
