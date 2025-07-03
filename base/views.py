1076b6fd1cbebd8e804cc6fd83944a4cad4c9bba
RadinAjoodani <radin.ajoodani@gmail.com>
2025-07-04 01:25:54 +0330

    remove manual rooms

--- base/views.py
+++ base/views.py
@@ -3,11 +3,6 @@ from django.http import HttpResponse
 from .models import Room
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name':'radin\'s room'},
-    {'id':2, 'name':'kitchen'},
-    {'id':3, 'name':'bathroom'}
-]
 
 def home(request):
     rooms = Room.objects.all()
@@ -15,9 +10,6 @@ def home(request):
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request, 'base/room.html', context)
