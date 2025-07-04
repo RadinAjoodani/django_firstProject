ac165e30e0f9959b867b195732909abaf9ba4a6c
RadinAjoodani <radin.ajoodani@gmail.com>
2025-07-04 13:54:12 +0330

    RoomForm added

--- base/models.py
+++ base/models.py
@@ -9,8 +9,11 @@ class Room(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(null=True)
     # participants = 
-    update = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.name
