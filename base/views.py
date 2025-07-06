71c28949fdbc378239fe0e733795cab0d1867f69
RadinAjoodani <radin.ajoodani@gmail.com>
2025-07-07 00:29:54 +0330

    delete messga and join to participants added

--- base/views.py
+++ base/views.py
@@ -77,6 +77,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
 
     if request.method == 'POST':
         message = Message.objects.create(
@@ -84,8 +85,11 @@ def room(request, pk):
             room = room,
             body = request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
-    context = {'room':room, 'room_messages':room_messages}
+    
+    context = {'room':room, 'room_messages':room_messages,
+                'participants':participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
@@ -128,3 +132,15 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj':room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    
+    if request.user != message.user:
+        return HttpResponse('You are not the host')
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':message})
